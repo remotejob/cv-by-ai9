305d9d87535a639acdc5102660d795324b82446b
Aleksander Mazurov <aleksander.mazurov@gmail.com>
2025-09-17 02:50:47 +0300

    Dark

--- frontend/app/(site)/layout.tsx
+++ frontend/app/(site)/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <body className="font-sans">
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
